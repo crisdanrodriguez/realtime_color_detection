a83808781b6d8ccdccc181a0d72e9d682df94324
Cristian <cristian.714@hotmail.com>
2022-01-27 00:25:33 -0600

    Documentation

--- realtime_color_detection.py
+++ realtime_color_detection.py
@@ -1,17 +1,22 @@
+# Import libraries
 import numpy as np
 import cv2
 
+# Capturing video through webcam
 webcam = cv2.VideoCapture(0)
 
 while True:
 
+    # Reading the video from the webcam in image frames
     ret, frame = webcam.read()
 
+    # Show the readed frames
     cv2.imshow('frame', frame)
 
+    # Condition for finishing the video capture when pressing 'q' key
     if cv2.waitKey(1) == ord('q'):
         break
 
-
+# Program termination
 webcam.release()
 cv2.destroyAllWindows
